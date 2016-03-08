e3c77974aa96d397d4808c24a48b134bb41a48f4
Luigi Sison <lsison@moxylus.com>
2016-03-07 23:13:31 -0800

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'To-Do Application',    
+    'description': 'Manage your personal Tasks with this module.',    
+    'author': 'Daniel Reis',    
+    'depends': ['mail'],    
+    'application': True, 
+} 
