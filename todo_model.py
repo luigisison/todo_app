8999d093bafa3f138fa5c9108b4c384d8b00e89a
Luigi Sison <lsison@moxylus.com>
2016-03-10 15:30:54 -0800

    Update todo_model.py

--- todo_model.py
+++ todo_model.py
@@ -6,7 +6,13 @@ class TodoTask(models.Model):
     is_done = fields.Boolean('Done?')    
     active = fields.Boolean('Active?', default=True)
 
-    @api.one
+    @api.multi
     def do_toggle_done(self):
         self.is_done = not self.is_done
         return True
+        
+    @api.multi
+    def do_clear_done(self):
+        done_recs = self.search([('is_done', '=', True)])
+        done_recs.write({'active': False})
+        return True    
