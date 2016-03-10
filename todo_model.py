4886b1a811a936f043da3b3dd3626f4c3ca9df61
Luigi Sison <lsison@moxylus.com>
2016-03-10 14:48:15 -0800

    Update todo_model.py

--- todo_model.py
+++ todo_model.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*
 
 <!-- api enables business logic for the buttons -->
-from openerp import models, fields, api 
+from openerp import models, fields, api
+
+<!-- todo.task model -->
 class TodoTask(models.Model):    
     _name = 'todo.task'    
     name = fields.Char('Description', required=True)    
     is_done = fields.Boolean('Done?')    
     active = fields.Boolean('Active?', default=True)
-    
-    <!-- Logic for the Toggle Done button -->
-    @api.one
-    def do_toggle_done(self):
-        self.is_done = not self.is_done
-        return True
